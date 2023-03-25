6c8c2eec43936b47a641193f77063fb989d5e7bd
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-25 13:24:17 +0600

    basic admin panal edit done

--- iblogs/blog/admin.py
+++ iblogs/blog/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from . models import Category, Post
 
+
+# for configuration of category admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('image_tag','title', 'descriptions', 'url', 'add_date')
+    search_fields = ('title',)
+
+
+
+# for configuration of post admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('image_tag','title', 'url')
+    search_fields = ('title',)
+    list_filter = ('cat',)
+
+
+
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- iblogs/blog/models.py
+++ iblogs/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -12,6 +13,12 @@ class Category(models.Model):
     image = models.ImageField(upload_to= 'category/')
     add_date = models.DateTimeField(auto_now_add= True, null= True)
 
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return format_html('<img src = "/media/{}"  style = "width: 40px; max-height: 40px;"  />'.format(self.image))
+
 
 
 # Post Model
@@ -22,3 +29,9 @@ class Post(models.Model):
     url = models.CharField(max_length= 100)
     cat = models.ForeignKey(Category, on_delete= models.CASCADE)
     image = models.ImageField(upload_to= 'post/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return format_html('<img src = "/media/{}"  style = "width: 40px; max-height: 40px;"  />'.format(self.image))
