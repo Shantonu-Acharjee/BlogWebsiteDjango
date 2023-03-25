c628ac87508df12240d4ece0dbcf06092042803e
Shantonu-Acharjee <shantonuacharjee@gmail.com>
2023-03-25 10:14:49 +0600

    error not solve

--- user_profile/managers.py
+++ user_profile/managers.py
@@ -1,26 +1,27 @@
 from django.contrib.auth.base_user import BaseUserManager
 
-class CustomerUserManager(BaseUserManager):
 
+class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password, **extra_fields):
-        
         if not username:
-            raise ValueError('The user must be set')
-        
+            raise ValueError("The user must be set")
+
         if not email:
-            raise ValueError('The email must be set')
-        
+            raise ValueError("The email must be set")
+
         if not password:
-            raise ValueError('The password must be set')
+            raise ValueError("The password must be set")
 
         email = self.normalize_email(email)
-        user = self.model(username, email, password, **extra_fields)
+        user = self.model(
+            username=username,
+            email=email,
+            password=password,
+            **extra_fields
+        )
         user.set_password(password)
         user.save()
-
-
-
-
+        return user
 
     def create_superuser(self, username, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
@@ -28,13 +29,14 @@ class CustomerUserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError('The super user must have is_staff=True')
-        
+            raise ValueError("The superuser must have is_staff=True")
+
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('The super user must have is_superuser=True')
-        
-        #if extra_fields.get('is_active') is not True:
-        #    raise ValueError('The super user must have is_active=True')
+            raise ValueError("The superuser must have is_superuser=True")
 
-
-        return self.create_user(username, email, password, extra_fields)
+        return self.create_user(
+            username,
+            email,
+            password,
+            **extra_fields
+        )
